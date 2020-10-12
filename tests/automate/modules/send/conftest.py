5ba85b61226f2a200095c6ac88c7ca22ec81d8d4
97gushan <97gustavh@gmail.com>
2020-10-12 14:47:05 +0200

    tests of checking and fetching of emails

---
+++ tests/automate/modules/send/conftest.py
@@ -0,0 +1,40 @@
+import sys
+import pytest
+
+from lib.automate.modules.send import Send, SETTINGS
+
+
+TEST_SETTINGS = {
+    "users": {
+        "First Last": {
+            "email": {
+                "address": "first.last@email.com",
+                "username": "first.last@email.com",
+                "password": "password",
+                "ssl": True,
+                "host": "localhost",
+                "port": 465,
+            }
+        },
+        "John Doe": {
+            "email": {
+                "address": "johndoe@email.com",
+                "username": None,
+                "password": None,
+                "ssl": False,
+                "host": "localhost",
+                "port": 1025,
+            }
+        },
+    },
+}
+
+
+@pytest.fixture
+def get_test_email(monkeypatch):
+    def helper(name):
+        send = Send()
+        monkeypatch.setattr("lib.automate.modules.send.SETTINGS", TEST_SETTINGS)
+        return send.get_email(name)
+
+    yield helper
