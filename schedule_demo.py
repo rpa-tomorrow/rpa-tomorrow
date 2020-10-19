d04466b0ebd200888f915fd1be1f1bf1388019a6
banunkers <hugwan-6@student.ltu.se>
2020-10-19 11:00:57 +0200

    Use automation module directly in schedule demo

--- schedule_demo.py
+++ schedule_demo.py
@@ -3,6 +3,7 @@ import sys
 from lib import Error
 from lib.automate import Automate
 from lib.nlp import nlp
+from datetime import datetime, timedelta
 
 automate = Automate()
 n = nlp.NLP("en_rpa_simple")
@@ -10,7 +11,12 @@ n = nlp.NLP("en_rpa_simple")
 # Enable the CalDav server before running the following
 try:
     text = "set meeting with Niklas at 20:00"
-    response = n.run(text)
+    # Have to use automation module directly because the NLP model does not
+    # support Schedule yet
+    # response = n.run(text)
+    when = datetime.now() + timedelta(hours=5.0)  # timestamp at: now + 5 hrs
+    response = automate.run("schedule", "rpa@substorm.com", when, "meeting", "Niklas")
+
     print(response)
 except Error as err:
     print(err, file=sys.stdout)
