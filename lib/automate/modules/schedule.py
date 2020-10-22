6ba6f6fd12baeb0a1b62bcff4c4368bba397317a
Niklas <nicke.l@telia.com>
2020-10-22 09:35:11 +0200

    Google calendar (#66)
    * Implemented support for adding event to Google Calander
    
    * Corrected the start and end time
    
    * Fixed the schedule demo and some other small improvements
    
    * Added a short guide to setup the Google Calander API
    
    * Removed the how to on local CalDav server

--- lib/automate/modules/schedule.py
+++ lib/automate/modules/schedule.py
@@ -1,18 +1,20 @@
-from datetime import datetime
-from datetime import timedelta
-
-from fuzzywuzzy import process as fuzzy
-
+from __future__ import print_function
 from lib.automate.modules import Module, NoSenderError
 from lib.settings import SETTINGS
+from fuzzywuzzy import process as fuzzy
+from datetime import datetime, timedelta
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-import caldav
-
-time_format = "%Y%m%dT%H%M%SZ"
+# If modifying these scopes, delete the files *.pickle.
+SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
 
 
 class Schedule(Module):
-    verbs = ["book", "schedule", "meeting", "set"]
+    verbs = ["book", "schedule", "meeting"]
 
     def __init__(self):
         super(Schedule, self).__init__()
@@ -34,38 +36,37 @@ class Schedule(Module):
                 "Could not parse date to schedule to.\nPlease enter date in YYYYMMDD HH:MM format",
             )
 
-        user, _ = fuzzy.extractOne(sender, SETTINGS["users"].keys())
+        if not body:
+            self.followup_type = "body"
+            return None, "Found no event summary. What is the event about"
+
+        user, _ = fuzzy.extractOne(self.sender, SETTINGS["users"].keys())
         settings = SETTINGS["users"][user]["email"]
         username = settings.get("username")
-        password = settings.get("password")
-        url = f'http://{settings["host"]}:{settings["port"]}/'
 
-        duration = 20  # TODO: Parse from body.
-        summary = "rpc-tomorrow meeting"  # TODO: Parse from body.
+        summary = self.body
 
-        client = caldav.DAVClient(url=url, username=username, password=password)
-        my_principal = client.principal()
+        # Parse Time
+        duration = 20  # TODO: Parse from input
+        start_time = self.when.isoformat() + "Z"  # 'Z' indicates UTC time
+        end_time = (self.when + timedelta(minutes=duration)).isoformat() + "Z"  # 'Z' indicates UTC time
 
-        my_new_calendar = my_principal.make_calendar(name="Test calendar")
+        # Define the event
+        event = {
+            "summary": summary,
+            "start": {"dateTime": start_time},
+            "end": {"dateTime": end_time},
+            "attendees": self.parse_attendees(settings["address"], self.to),
+        }
 
-        start = when.strftime(time_format)
-        end = (when + timedelta(minutes=duration)).strftime(time_format)
-        now = datetime.now().strftime(time_format)
-        my_event = my_new_calendar.save_event(
-            f"""BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//rpa-tomorrow.//CalDAV Client//EN
-BEGIN:VEVENT
-UID:{now}-{settings["address"]}
-DTSTART:{start}
-DTEND:{end}
-SUMMARY:{summary}
-END:VEVENT
-END:VCALENDAR
-"""
-        )
+        # Get or create user credentials
+        creds = self.credentials(username)
 
-        return my_event.data, None
+        # Create Event using Google calendar API
+        service = build("calendar", "v3", credentials=creds)
+        event = service.events().insert(calendarId="primary", body=event).execute()
+
+        return "Event created, see link: %s" % (event.get("htmlLink")), None
 
     def followup(self, answer):
         """
@@ -78,5 +79,44 @@ END:VCALENDAR
             except Exception:
                 when = None
             return self.run(self.to, when, self.body, self.sender)
+        elif self.followup_type == "body":
+            return self.run(self.to, self.when, answer, self.sender)
         else:
             raise NotImplementedError("Did not find any valid followup question to answer.")
+
+    def credentials(self, username):
+        """
+        The file token.pickle stores the user's access and refresh tokens, and is
+        created automatically when the authorization flow completes for the first
+        time.
+        """
+        creds = None
+        pickle_filename = f"{username}_token.pickle"
+
+        if os.path.exists(pickle_filename):
+            with open(pickle_filename, "rb") as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(pickle_filename, "wb") as token:
+                pickle.dump(creds, token)
+
+        return creds
+
+    def parse_attendees(self, sender_address, to):
+        """
+        Parses the attendees of the event and creates a list of dicts containing
+        there emails.
+        """
+        attendees = []
+        attendees.append({"email": sender_address})
+        for attende in to:
+            attendees.append({"email": attende})
+
+        return attendees
