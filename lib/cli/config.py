6b92040fcef01de6df6d610033a81f4fea8c0a19
97gushan <97gustavh@gmail.com>
2020-10-22 09:15:01 +0200

    add function docstrings

--- lib/cli/config.py
+++ lib/cli/config.py
@@ -1,21 +1,24 @@
 import sys
 
 
-def config_user_name():
+def config_user_name() -> str:
+    """ Prompt the user about entering a name and return that name """
     print("User not configured.\nPlease enter your name: ")
     name = sys.stdin.readline().strip()
 
     return name
 
 
-def config_email_address():
+def config_email_address() -> str:
+    """ Prompt the user about entering a email address and return that address """
     print("Email address not configured.\nPlease enter your email: ")
     email = sys.stdin.readline().strip()
 
     return email
 
 
-def config_email_host(email_config):
+def config_email_host(email_config: dict) -> dict:
+    """ Prompt the user about entering email host information and return this information """
     print("Email host not configured.\nPlease enter host: ")
     email_config["host"] = sys.stdin.readline().strip()
     print("Enter Port: ")
--- lib/settings.py
+++ lib/settings.py
@@ -5,28 +5,32 @@ SETTINGS = {}
 
 
 def load_settings():
+    """ Load settings from the config files located in /config"""
     load_user()
-
     load_local_contacts()
 
 
 def load_user():
+    """ Load user information from the config file
+        if there are user settings missing then the user
+        is prompted to input these """
     with open("config/user.yaml", "r") as stream:
         SETTINGS["user"] = yaml.safe_load(stream)
 
+    # Flag for checking if there is a need to update the config by writing to a file
     update = False
 
     if SETTINGS["user"]["name"] == "":
         SETTINGS["user"]["name"] = config_user_name()
         update = True
 
-    email = SETTINGS["user"]["email"]
-    if email["address"] == "":
-        email["address"] = config_email_address()
+    email_config = SETTINGS["user"]["email"]
+    if email_config["address"] == "":
+        email_config["address"] = config_email_address()
         update = True
 
-    if email["host"] == "":
-        email = config_email_host(email)
+    if email_config["host"] == "":
+        email_config = config_email_host(email_config)
         update = True
 
     if update:
@@ -35,10 +39,12 @@ def load_user():
 
 
 def load_local_contacts():
+    """ Loads the local contact book from the config file """
     with open("config/contacts.yaml", "r") as stream:
         SETTINGS["contacts"] = yaml.safe_load(stream)
 
 
-def update_settings(file_path, data):
+def update_settings(file_path: str, data: dict):
+    """Writes to a yaml config file"""
     with open(file_path + ".yaml", "w", encoding="utf8") as outfile:
         yaml.dump(data, outfile)
