4af369ce50ebef708d701091b30dc15b5f2e19eb
Mark Hakansson <mark.hakansson@live.se>
2020-10-23 08:01:11 +0200

    add script for installing all models

---
+++ scripts/install_nlp_models.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install https://github.com/rpa-tomorrow/model-releases/releases/download/en_rpa_simple_reminder-0.0.1/en_rpa_simple_reminder-0.0.1.tar.gz
+
+pip install https://github.com/rpa-tomorrow/model-releases/releases/download/en_rpa_simple_email-0.0.1/en_rpa_simple_email-0.0.1.tar.gz
+
+pip install https://github.com/rpa-tomorrow/model-releases/releases/download/en_rpa_simple-0.0.1/en_rpa_simple-0.0.1.tar.gz
+
