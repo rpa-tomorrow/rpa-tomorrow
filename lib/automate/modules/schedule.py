4cda6ca29adedf10b071ab84348032f7c06db4a6
Aron Widforss <aron@antarkt.is>
2020-10-23 11:48:38 +0200

    Check if users are busy when an event is created. (#68)
    * Check if sender is busy when an event is created.
    
    * Follow lint suggestions
    
    * Check if receiver is busy during meeting

--- lib/automate/modules/schedule.py
+++ lib/automate/modules/schedule.py
@@ -8,7 +8,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the files *.pickle.
-SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
+SCOPES = [
+    "https://www.googleapis.com/auth/calendar.events.owned",
+    "https://www.googleapis.com/auth/calendar.readonly",
+]
 
 
 class Schedule(Module):
@@ -55,12 +58,35 @@ class Schedule(Module):
             "end": {"dateTime": end_time},
             "attendees": self.parse_attendees(settings["address"], self.to),
         }
+        self.event = event
 
         # Get or create user credentials
         creds = self.credentials(username)
 
         # Create Event using Google calendar API
         service = build("calendar", "v3", credentials=creds)
+        self.service = service
+
+        # Check if we are busy
+        to_items = [{"id": email} for email in to]
+        freebusy = (
+            service.freebusy()
+            .query(body={"items": [{"id": "primary"}] + to_items, "timeMin": start_time, "timeMax": end_time})
+            .execute()
+        )
+        to_busy = list(map(lambda x: x[0], filter(lambda x: x[1]["busy"], list(freebusy["calendars"].items())[1:])))
+
+        other = f"{', '.join(to_busy[:-1])} and {to_busy[-1]}" if len(to_busy) > 1 else "".join(to_busy)
+        if len(freebusy["calendars"]["primary"]["busy"]) and len(to_busy):
+            self.followup_type = "both_busy"
+            return None, f"You as well as {other} seem to be busy. Do you want to book the meeting anyway? [Y/n]"
+        elif len(freebusy["calendars"]["primary"]["busy"]):
+            self.followup_type = "self_busy"
+            return None, "You seem to be busy during this meeting. Do you want to book it anyway? [Y/n]"
+        elif len(to_busy):
+            self.followup_type = "to_busy"
+            return None, f"{other} seem to be busy during this meeting. Do you want to book it anyway? [Y/n]"
+
         event = service.events().insert(calendarId="primary", body=event).execute()
 
         return "Event created, see link: %s" % (event.get("htmlLink")), None
@@ -78,6 +104,14 @@ class Schedule(Module):
             return self.run(self.to, when, self.body, self.sender)
         elif self.followup_type == "body":
             return self.run(self.to, self.when, answer, self.sender)
+        elif self.followup_type == "self_busy" or self.followup_type == "both_busy" or self.followup_type == "to_busy":
+            if answer == "" or answer.lower() == "y" or answer.lower() == "yes":
+                event = self.service.events().insert(calendarId="primary", body=self.event).execute()
+                return "Event created, see link: %s" % (event.get("htmlLink")), None
+            elif answer.lower() == "n" or answer.lower() == "no":
+                return "No event created", None
+            else:
+                return self.run(self.to, self.when, self.body, self.sender)
         else:
             raise NotImplementedError("Did not find any valid followup question to answer.")
 
