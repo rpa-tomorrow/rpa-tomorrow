a9bafccfb0e9c55d097fd5e5a8661c08b4843b8b
Mark Hakansson <mark.hakansson@live.se>
2020-10-23 08:00:57 +0200

    reformat reminder and email module to use split models

--- lib/automate/modules/reminder.py
+++ lib/automate/modules/reminder.py
@@ -41,6 +41,10 @@ class Reminder(Module):
             pass
 
     def run(self, text, sender):
+        to, when, body = self.nlp(text)
+        return self.execute_task(to, when, body, sender)
+
+    def execute_task(self, _to, when, body, _sender):
         """
         Schedules a reminder which will show up in the users system at the
         specified time with the specified information. Raises an error if the
@@ -48,31 +52,32 @@ class Reminder(Module):
         reminder is in the past.
 
         :param _to: Unused
-        :param _sender: Unused
         :param when: The time to schedule the notification for
         :type when: datetime.datetime
         :param body: The text to be shown in the notification
         :type body: text
+        :param _sender: Unused
         """
-        self.to, self.when, self.body = self.nlp(text)
-
-        self.sender = sender
+        self.to = _to
+        self.when = when
+        self.body = body
+        self.sender = _sender
         self.followup_type = None
 
-        if not isinstance(self.when, datetime):
+        if not isinstance(when, datetime):
             self.followup_type = "when"
             return (
                 None,
                 "Could not parse date to schedule to.\nPlease enter date in YYYYMMDD HH:MM format",
             )
-        elif not self.body:
+        elif not body:
             self.followup_type = "body"
             return None, "Found no message body. What message should be sent"
 
-        when_delta = (self.when - datetime.now()).total_seconds()  # convert to difference in seconds
+        when_delta = (when - datetime.now()).total_seconds()  # convert to difference in seconds
         if when_delta < 0.0:
             raise TimeIsInPastError(
-                self.when.strftime("%Y-%m-%d %H:%M:%S"),
+                when.strftime("%Y-%m-%d %H:%M:%S"),
                 "The specified time of the reminder is in the past and can not" + " be scheduled",
             )
 
@@ -83,10 +88,10 @@ class Reminder(Module):
                 + " it is currently not supported by the program",
             )
 
-        t = Timer(when_delta, lambda: self.notify(sys.platform, self.body))
+        t = Timer(when_delta, lambda: self.notify(sys.platform, body))
         t.start()
         return (
-            f"Reminder scheduled for {self.when.strftime('%Y-%m-%d %H:%M:%S')}",
+            f"Reminder scheduled for {when.strftime('%Y-%m-%d %H:%M:%S')}",
             None,
         )
 
@@ -100,15 +105,16 @@ class Reminder(Module):
                 when = datetime.fromisoformat(answer)
             except Exception:
                 when = None
-            return self.run(self.to, self.when, self.body, self.sender)
+            return self.execute_task(self.to, self.when, self.body, self.sender)
 
         elif self.followup_type == "body":
-            return self.run(self.to, self.when, answer, self.sender)
+            return self.execute_task(self.to, self.when, answer, self.sender)
         else:
             raise NotImplementedError("Did not find any valid followup question to answer.")
 
     def nlp(self, text):
         """
+        Lets the reminder model work on the given text.  
         """
         nlp = spacy.load("en_rpa_simple_reminder")
         doc = nlp(text)
