835dc2622ba855680de7cbe746e13471963732ee
Viktor From <viktor.from91@gmail.com>
2020-10-28 13:17:44 +0100

    Revert changes

--- lib/automate/__init__.py
+++ lib/automate/__init__.py
@@ -1,33 +1,77 @@
-class Error(Exception):
+from fuzzywuzzy import process as fuzzy
+from importlib import import_module
+
+from lib import Error
+from lib.settings import SETTINGS
+
+
+class Automate:
+    def __init__(self):
+        """ Register new modules here """
+        self.modules = [
+            import_module("lib.automate.modules.send").Send,
+            import_module("lib.automate.modules.schedule").Schedule,
+            import_module("lib.automate.modules.reminder").Reminder,
+        ]
+        self.verbs = {}
+        for module in self.modules:
+            for verb in module.verbs:
+                self.verbs[verb] = module
+
+    def get_verbs(self):
+        """
+        :return: A list of keywords for the registered automation modules.
+        :rtype: list[string]
+        """
+        return list(self.verbs.keys())
+
+    def get_senders(self):
+        """
+        :return: A list of names of users listed in the SETTINGS.
+        :rtype: list[string]
+        """
+        return list(SETTINGS["users"].keys())
+
+    def run(self, module_name, to, when, body):
+        """
+        Run automation on registered module.
+        :param module_name: Something close to a keyword of a automation
+                            module. This is handled via fuzzy search.
+        :type module_name: string
+        :param to: Intended recipients. Should be fuzzy in the future.
+        :type to: list[string]
+        :param when: When the command should be executed. Not currently used.
+        :type when: datetime.datetime
+        :param body: A body text of the task, if relevant.
+        :type body: string
+        :return: Response from automation module.
+        :rtype: string
+        """
+        sender = SETTINGS["user"]
+
+        def handle_response(success, response):
+            if response:
+                print(response, end=": ", flush=True)
+                return handle_response(*instance.followup(input()))
+            elif success:
+                return success
+            else:
+                raise NoResponseError("No response from automation module")
+
+        if module_name in self.verbs:
+            instance = self.verbs[module_name]()
+        else:
+            fuzzy_match, score = fuzzy.extractOne(module_name, self.get_verbs())
+            if score > 30:
+                instance = self.verbs[fuzzy_match]()
+            else:
+                raise AutomationNotFoundError("Automation module not found")
+        return handle_response(*instance.run(to, when, body, sender))
+
+
+class NoResponseError(Error):
     pass
 
 
-class NotImplementedError(Error):
+class AutomationNotFoundError(Error):
     pass
-
-
-class TimeIsInPastError(Error):
-    """
-    Raised when the time that something is supposed to happen has already
-    passed.
-    """
-
-    def __init__(self, time, message="The specifed time is in the past"):
-        self.time = time
-        self.message = message
-        super().__init__(self.message)
-
-    def __str__(self):
-        return f"{self.message}, time={self.time}"
-
-
-class OSNotSupportedError(Error):
-    """Raised when a feature is not currently supported on the host OS"""
-
-    def __init__(self, os, message="The feature is not supported on the current OS"):
-        self.os = os
-        self.message = message
-        super().__init__(self.message)
-
-    def __str__(self):
-        return f"{self.message}, os={self.os}"
