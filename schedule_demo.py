f3d73b28f9d8de48e193675b0e0dfa37548916fd
Mark Hakansson <mark.hakansson@live.se>
2020-10-29 09:07:44 +0100

    update schedule demo

--- schedule_demo.py
+++ schedule_demo.py
@@ -12,12 +12,8 @@ n = nlp.NLP("en_rpa_simple")
 
 # Enable the CalDav server before running the following
 try:
-    text = "set meeting with Niklas at 20:00"
-    # Have to use automation module directly because the NLP model does not
-    # support Schedule yet
-    # response = n.run(text)
-    when = datetime.utcnow() + timedelta(hours=5.0)  # timestamp at: now + 5 hrs
-    response = automate.run("schedule", ["rpa@substorm.com"], when, "Schedule demo")
+    text = "schedule meeting with Niklas at 20:00"
+    response = n.run(text)
 
     print(response)
 except Error as err:
