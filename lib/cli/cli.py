09fa3da0623bbab1a1a9958398d9230210ea2a2b
Mark Hakansson <mark.hakansson@live.se>
2020-11-03 09:16:26 +0100

    add debug flag to cli

--- lib/cli/cli.py
+++ lib/cli/cli.py
@@ -1,13 +1,41 @@
+import plac
 import sys
 import commands
+import logging
 from lib.settings import load_settings
-
 from lib.nlp import nlp  # noqa: E402
 
 MODEL_NAME = "en_rpa_simple"
 
 
-def cli():
+def setup_logger(level):
+    # create logger
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+
+@plac.annotations(
+    debug=("Enable debug output", "flag", "d"),
+)
+def cli(debug):
+    if debug:
+        setup_logger(logging.DEBUG)
+    else:
+        setup_logger(logging.WARNING)
+
     load_settings()
     print("Loading...")
     n = nlp.NLP(MODEL_NAME)
@@ -24,4 +52,4 @@ def cli():
 
 
 if __name__ == "__main__":
-    cli()
+    plac.call(cli)
