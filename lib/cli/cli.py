d88a912a7bf313747d57d03277bc7a4635501947
Mark Hakansson <44841869+markhakansson@users.noreply.github.com>
2020-11-09 08:51:00 +0100

    Add variable debug option to CLI (#108)
    * add variable debug option to cli
    
    * fix bug where debug level would not be set

--- lib/cli/cli.py
+++ lib/cli/cli.py
@@ -26,12 +26,24 @@ def setup_logger(level):
     logger.addHandler(ch)
 
 
-@plac.annotations(debug=("Enable debug output", "flag", "d"))
-def cli(debug):
-    if debug:
-        setup_logger(logging.DEBUG)
-    else:
+@plac.annotations(
+    debug=("Set the debug logging level which will be output", "option", "d", str),
+    verbose=("Enable verbose output", "flag", "v"),
+)
+def cli(debug, verbose):
+    try:
+        if debug is not None and verbose:
+            print("WARNING: Verbose and debug output are both enabled. Ignoring verbose flag!")
+
+        if debug is not None:
+            setup_logger(debug)
+        elif verbose:
+            setup_logger(logging.INFO)
+        else:
+            setup_logger(logging.WARNING)
+    except ValueError:
         setup_logger(logging.WARNING)
+        print("WARNING: Could not parse debug flag. Using default log settings.")
 
     load_settings_from_cli()
     print("Loading...")
