72110a36ea35e247782c87e2e9667e232a81827f
Viktor From <viktor.from91@gmail.com>
2020-11-11 10:45:25 +0100

    Add support for interval scheduling

--- lib/automate/modules/schedule.py
+++ lib/automate/modules/schedule.py
@@ -23,12 +23,13 @@ class Schedule(Module):
     def prepare(self, nlp_model_names, text, sender):
         if self.nlp_model is None:
             self.nlp_model = spacy.load(nlp_model_names["schedule"])
-        to, when, body = self.nlp(text)
-        return self.prepare_processed(to, when, body, sender)
+        to, start, end, body = self.nlp(text)
+        return self.prepare_processed(to, start, end, body, sender)
 
-    def prepare_processed(self, to, when, body, sender):
+    def prepare_processed(self, to, start, end, body, sender):
         self.to = to
-        self.when = when
+        self.start = start
+        self.end = end
         self.body = body
         self.sender = sender
         self.followup_type = None
@@ -36,12 +37,14 @@ class Schedule(Module):
         self.uncertain_attendee = None
         self.unknown_attendee = None
 
+        # print("to = ", to, " start = ", start, " end = ", end,  " body = ", body, " sender = ", sender)
+
         if not sender:
             raise NoSenderError("No sender found!")
 
-        if not isinstance(when, datetime):
-            self.followup_type = "when"
-            return "Could not parse date to schedule to.\nPlease enter date in YYYYMMDD HH:MM format"
+        if not isinstance(start, datetime):
+            self.followup_type = "start"
+            return "Could not parse date to schedule start time.\nPlease enter date in YYYYMMDD HH:MM format"
 
         if not body:
             self.followup_type = "body"
@@ -54,7 +57,10 @@ class Schedule(Module):
         calendar = google.calendar(settings)
         self.calendar = calendar
 
-        duration = timedelta(minutes=20)  # TODO: Parse from input
+        # Parse Time
+        start_time = tc.local_to_utc_time(self.start).isoformat()
+        end_time = tc.local_to_utc_time(self.end).isoformat()
+
 
         # Parse attendees (try to resolve email addresses)
         parsed_attendees = get_emails(self.to, sender)
@@ -70,12 +76,21 @@ class Schedule(Module):
             return f"Found no contact named {name}. Do you want to continue scheduling the meeting anyway? [Y/n]"
 
         attendees = [settings["address"]] + attendees
-        event = calendar.event(when, duration, attendees, self.body)
+        event = calendar.event(start_time, end_time, attendees, self.body)
         self.event = event
 
+        print("event = ", event)
+
+        # Get or create user credentials
+        creds = self.credentials(username)
+
+        # Create Event using Google calendar API
+        service = build("calendar", "v3", credentials=creds, cache_discovery=False)
+        self.service = service
+
         # Check if we are busy
-        me_busy = calendar.freebusy(when, duration, ["primary"])
-        to_busy = calendar.freebusy(when, duration, to)
+        me_busy = calendar.freebusy(start_time, end_time, ["primary"])
+        to_busy = calendar.freebusy(start_time, end_time, to)
         other = f"{', '.join(to_busy[:-1])} and {to_busy[-1]}" if len(to_busy) > 1 else "".join(to_busy)
         if me_busy and to_busy:
             self.followup_type = "both_busy"
@@ -96,31 +111,39 @@ class Schedule(Module):
         Follow up method after the module have had to ask a question to clarify some parameter, or just
         want to check that it interpreted everything correctly.
         """
-        if self.followup_type == "when":
+        if self.followup_type == "start":
             try:
-                when = datetime.fromisoformat(answer)
+                start = datetime.fromisoformat(answer)
             except Exception:
-                when = None
-            return self.prepare_processed(self.to, when, self.body, self.sender)
+                start = None
+            return self.prepare_processed(self.to, start, self.body, self.sender)
         elif self.followup_type == "body":
-            return self.prepare_processed(self.to, self.when, answer, self.sender)
+            return self.prepare_processed(self.to, self.start, answer, self.sender)
         elif self.followup_type == "self_busy" or self.followup_type == "both_busy" or self.followup_type == "to_busy":
             if answer == "" or answer.lower() == "y" or answer.lower() == "yes":
                 return None
             elif answer.lower() == "n" or answer.lower() == "no":
                 raise ActionInterruptedByUserError("Interrupted due to busy attendees.")
             else:
-                return self.prepare_processed(self.to, self.when, self.body, self.sender)
+                return self.prepare_processed(self.to, self.start, self.body, self.sender)
         elif self.followup_type == "to_uncertain":
-            return followup_contact_choice(self, answer)
+            try:
+                choice = int(answer) - 1
+            except Exception:
+                return self.prepare_processed(self.to, self.start, self.body, self.sender)
+            name, candidates = self.uncertain_attendee
+            if choice >= 0 and choice < len(candidates):
+                self.to.remove(name)  # update to so recursive call continues resolving new attendees
+                self.to.append(candidates[choice][1])  # add email of chosen attendee
+            return self.prepare_processed(self.to, self.start, self.body, self.sender)
         elif self.followup_type == "to_unknown":
             if answer == "" or answer.lower() == "y" or answer.lower() == "yes":
                 self.to.remove(self.unknown_attendee)
-                return self.prepare_processed(self.to, self.when, self.body, self.sender)
+                return self.prepare_processed(self.to, self.start, self.body, self.sender)
             elif answer.lower() == "n" or answer.lower() == "no":
                 raise ActionInterruptedByUserError("Interrupted due to unknown attendee.")
             else:
-                return self.prepare_processed(self.to, self.when, self.body, self.sender)
+                return self.prepare_processed(self.to, self.start, self.body, self.sender)
         else:
             raise NotImplementedError("Did not find any valid followup question to answer.")
 
@@ -129,27 +152,34 @@ class Schedule(Module):
         doc = self.nlp_model(text)
 
         to = []
-        when = []
+        start = []
+        end = []
         body = []
 
         for token in doc:
             if token.dep_ == "TO":
                 to.append(token.text)
-            elif token.dep_ == "WHEN":
-                when.append(token.text)
+            elif token.dep_ == "START":
+                start.append(token.text)
+            elif token.dep_ == "END":
+                end.append(token.text)
             elif token.dep_ == "BODY":
                 body.append(token.text)
             log.debug("%s %s", token.text, token.dep_)
 
-        time = datetime.now()
-        if len(when) == 0:
-            time = time + timedelta(seconds=5)
+
+        time1 = datetime.now()
+        if len(start) == 0:
+            time1 = time1 + timedelta(seconds=5)
         else:
-            time = tc.parse_time(when)
+            time1 = tc.parse_time(start)
+
+        time2 = datetime.now()
+        time2 = tc.parse_time(end)
 
         _body = " ".join(body)
 
-        return (to, time, _body)
+        return (to, time1, time2, _body)
 
 
 class ActionInterruptedByUserError(Error):
