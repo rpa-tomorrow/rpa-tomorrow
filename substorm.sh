bbef734bbbc3eca3aee65c06fdcf0fc440b2228c
Mark Hakansson <mark.hakansson@live.se>
2020-11-12 10:08:31 +0100

    update environment and init script

--- substorm.sh
+++ substorm.sh
@@ -3,16 +3,15 @@ MULTILINE1=$(conda env list | grep -F substorm-nlp)
 
 if [ "$MULTILINE1" = "" ]
 then
-   echo "no conda env found, creating env..."
-   conda env create -f substorm-nlp.yml
+    echo "no conda env found, creating env..."
+    conda env create -f substorm-nlp.yml
 fi
 
 echo "activate conda substorm-nlp env..."
 source ~/anaconda3/etc/profile.d/conda.sh 
 conda activate substorm-nlp
 
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_md
+spacy download xx_ent_wiki_sm
 
 MULTILINE2=$(pip list | grep -F en-rpa-simple)
 
@@ -21,8 +20,8 @@ echo "${MULTILINE2}"
 
 if [ "$MULTILINE2" = "" ]
 then
-	echo "missing rpa-models, installing required packages..."
-	pip install -r requirements.txt
-	pwd
+    echo "missing rpa-models, installing required packages..."
+    pip install -r requirements.txt
+    pwd
 fi
 
