c6ed9d7928adbd53fc6462a78712b34dc0070ce6
97gushan <97gustavh@gmail.com>
2020-11-16 10:56:59 +0100

    Remove Calendar Events (#129)
    * remove schedule events automation module
    
    * check the followup question so that the user is sure to delete the event
    
    * fetch event based on time or summary depending on user input
    
    * display event datetime in a more readable way
    
    * find event by participant
    
    * refactor remove_schedule, fetch event by participants and handle multiple events
    
    * use the google module for finding and deletion of events

--- examples/remove_schedule_demo.py
+++ examples/remove_schedule_demo.py
@@ -4,17 +4,21 @@ sys.path.append(".")
 sys.path.append("..")
 
 from lib import Error  # noqa: E402
+from lib.automate import Automate  # noqa: E402
 from lib.nlp import nlp  # noqa: E402
+from datetime import datetime, timedelta  # noqa: E402
 from lib.settings import load_settings  # noqa: E402
 
 load_settings()
+automate = Automate()
 n = nlp.NLP("en_rpa_simple", "en_core_web_md")
 
 # Enable the CalDav server before running the following
 try:
-    text = "schedule meeting with john@email.com at 20:00"
+    when = datetime.now() + timedelta(hours=5.0)  # timestamp at: now + 5 hrs
+    text = "unschedule meeting with john"
+    print("input:", text)
     response = n.run(text)
-
     print(response)
 except Error as err:
     print(err, file=sys.stdout)
