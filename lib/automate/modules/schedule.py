ab0d9fc271f9074c26880c79237f4c57443c5ea1
hugowangler <hugwan-6@student.ltu.se>
2020-11-16 11:41:52 +0100

    Fix issue with followup questions

--- lib/automate/modules/schedule.py
+++ lib/automate/modules/schedule.py
@@ -23,13 +23,12 @@ class Schedule(Module):
     def prepare(self, nlp_model_names, text, sender):
         if self.nlp_model is None:
             self.nlp_model = spacy.load(nlp_model_names["schedule"])
-        to, start, end, body = self.nlp(text)
-        return self.prepare_processed(to, start, end, body, sender)
+        to, when, body = self.nlp(text)
+        return self.prepare_processed(to, when, body, sender)
 
-    def prepare_processed(self, to, start, end, body, sender):
+    def prepare_processed(self, to, when, body, sender):
         self.to = to
-        self.start = start
-        self.end = end
+        self.when = when
         self.body = body
         self.sender = sender
         self.followup_type = None
@@ -40,8 +39,8 @@ class Schedule(Module):
         if not sender:
             raise NoSenderError("No sender found!")
 
-        if not isinstance(self.start, datetime):
-            self.followup_type = "start"
+        if not isinstance(self.when["start"], datetime):
+            self.followup_type = "when"
             return "Could not parse date to schedule to.\nPlease enter date in YYYYMMDD HH:MM format"
 
         if not body:
@@ -69,12 +68,12 @@ class Schedule(Module):
             return f"Found no contact named {name}. Do you want to continue scheduling the meeting anyway? [Y/n]"
 
         attendees = [settings["address"]] + attendees
-        event = calendar.event(self.start, self.end, attendees, self.body)
+        event = calendar.event(self.when["start"], self.when["end"], attendees, self.body)
         self.event = event
 
         # Check if we are busy
-        me_busy = calendar.freebusy(self.start, self.end, ["primary"])
-        to_busy = calendar.freebusy(self.start, self.end, to)
+        me_busy = calendar.freebusy(self.when["start"], self.when["end"], ["primary"])
+        to_busy = calendar.freebusy(self.when["start"], self.when["end"], to)
         other = f"{', '.join(to_busy[:-1])} and {to_busy[-1]}" if len(to_busy) > 1 else "".join(to_busy)
         if me_busy and to_busy:
             self.followup_type = "both_busy"
@@ -95,31 +94,31 @@ class Schedule(Module):
         Follow up method after the module have had to ask a question to clarify some parameter, or just
         want to check that it interpreted everything correctly.
         """
-        if self.followup_type == "start":
+        if self.followup_type == "when":
             try:
-                self.start = datetime.fromisoformat(answer)
+                self.when = {"start": datetime.fromisoformat(answer), "end": None}
             except Exception:
-                self.start = None
-            return self.prepare_processed(self.to, self.start, self.body, self.sender)
+                self.when = None
+            return self.prepare_processed(self.to, self.when, self.body, self.sender)
         elif self.followup_type == "body":
-            return self.prepare_processed(self.to, self.start, answer, self.sender)
+            return self.prepare_processed(self.to, self.when, answer, self.sender)
         elif self.followup_type == "self_busy" or self.followup_type == "both_busy" or self.followup_type == "to_busy":
             if answer == "" or answer.lower() == "y" or answer.lower() == "yes":
                 return None
             elif answer.lower() == "n" or answer.lower() == "no":
                 raise ActionInterruptedByUserError("Interrupted due to busy attendees.")
             else:
-                return self.prepare_processed(self.to, self.start, self.body, self.sender)
+                return self.prepare_processed(self.to, self.when, self.body, self.sender)
         elif self.followup_type == "to_uncertain":
             return followup_contact_choice(self, answer)
         elif self.followup_type == "to_unknown":
             if answer == "" or answer.lower() == "y" or answer.lower() == "yes":
                 self.to.remove(self.unknown_attendee)
-                return self.prepare_processed(self.to, self.start, self.body, self.sender)
+                return self.prepare_processed(self.to, self.when, self.body, self.sender)
             elif answer.lower() == "n" or answer.lower() == "no":
                 raise ActionInterruptedByUserError("Interrupted due to unknown attendee.")
             else:
-                return self.prepare_processed(self.to, self.start, self.body, self.sender)
+                return self.prepare_processed(self.to, self.when, self.body, self.sender)
         else:
             raise NotImplementedError("Did not find any valid followup question to answer.")
 
@@ -157,7 +156,7 @@ class Schedule(Module):
 
         _body = " ".join(body)
 
-        return (to, start_time, end_time, _body)
+        return (to, {"start": start_time, "end": end_time}, _body)
 
 
 class ActionInterruptedByUserError(Error):
