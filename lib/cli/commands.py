d1562221924d1ad7889635bf143df003a5375492
Mark Hakansson <mark.hakansson@live.se>
2020-11-24 10:11:49 +0100

    update function names and format

--- lib/cli/commands.py
+++ lib/cli/commands.py
@@ -5,6 +5,7 @@ The commands available in the CLI
 import sys
 import os
 import pyaudio
+import click
 from config import config_model_language, choose_version
 
 sys.path.append(".")
@@ -14,14 +15,16 @@ from lib.settings import SETTINGS, get_model_languages, update_settings, get_lan
 from lib.speech.transcribe import transcribe  # noqa: E402
 
 
-def commands(arr, nlp):
+def commands(arr, selector):
     """
     Commands supported by the CLI
     """
     if arr[0] == "exit" or arr[0] == "e":
+        click.secho("\nGood bye!", fg="green", bold=True)
+        click.clear()
         sys.exit()
     elif arr[0] == "help" or arr[0] == "h":
-        prompt()
+        help_output()
     elif arr[0] == "language" or arr[0] == "lang":
         SETTINGS["user"]["language"] = config_model_language(get_model_languages())
         SETTINGS["user"]["language_version"] = choose_version(get_language_versions(SETTINGS["user"]["language"]))
@@ -34,33 +37,34 @@ def commands(arr, nlp):
     elif arr[0] == "record" or arr[0] == "r":
         # create audio interface and inform user that the output can be ignored
         audio_interface = pyaudio.PyAudio()
-        print(
+        click.echo(
             "\nIf you see warning messages above they can be ignored. It's caused by PyAudio"
             + " (used for micrpohone input) and cannot be removed.\n"
         )
-        print("Recording, please speak...")
-        transcribedInput = transcribe(audio_interface)
-        ans = input(f"This is what I heard:\n    {transcribedInput}\nExecute? [Y/n]: ").lower()
+        click.echo("Recording, please speak...")
+        transcribed_input = transcribe(audio_interface)
+        audio_interface.terminate()
+        ans = input(f"This is what I heard:\n    {transcribed_input}\nExecute? [Y/n]: ").lower()
         if ans == "y" or ans == "yes" or ans == "":
-            run_NLP(transcribedInput, nlp)
+            run_selector(transcribed_input, selector)
         else:
-            print("Canceled.")
+            click.echo("Canceled.")
     else:
         text = " ".join(map(str, arr))
-        run_NLP(text, nlp)
+        run_selector(text, selector)
 
 
-def prompt():
+def help_output():
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, "helpprompt.txt")
     f = open(filename, "r")
     print(f.read())
 
 
-def run_NLP(input, nlp):
+def run_selector(input, selector):
     try:
-        responses = nlp.run(input)
+        responses = selector.run(input)
         for response in responses:
-            print(response + "\n")
+            click.echo(response + "\n")
     except Exception as e:
         print("Failed to execute action.\n", e, file=sys.stderr)
