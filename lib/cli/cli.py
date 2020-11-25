e2cdbd8dce133ae76925dbcf91a6f55ba8644317
Mark Hakansson <44841869+markhakansson@users.noreply.github.com>
2020-11-25 13:55:19 +0100

    Change CLI library and make CLI more intuitive (#163)
    * remove spacy similarity warning from cli
    
    * change CLI lib, improve CLI output
    
    * make print before spinner optional
    
    * update function names and format
    
    * add information about help command in cli
    
    * update environment file
    
    * remove double log
    
    * ignore code styling in workflows
    
    * ingore line break before bin op error in flake8
    
    * add config for flake8

--- lib/cli/cli.py
+++ lib/cli/cli.py
@@ -1,14 +1,82 @@
-import plac
-import sys
 import commands
 import logging
 import spinner
+import warnings
+import click
 
 from config import load_settings_from_cli
 from lib.selector.selector import ModuleSelector  # noqa: E402
 
 
-def setup_logger(level):
+@click.command()
+@click.option("--verbose", "-v", "verbose", is_flag=True, help="Enable verbose output")
+@click.option("--debug", "-d", "debug", help="Set the debug logging level which will be output")
+def cli(debug, verbose):
+    click.clear()
+    click.secho("RPA Tomorrow - managing tasks from natural text", fg="green", bold=True)
+
+    spin = spinner.Spinner()
+    load_prerequisites(spin, debug, verbose)
+    n = load_selector(spin)
+
+    click.secho("\nType any task(s) and the robot will start working on it", bold=True)
+    click.secho("(or type 'help' to display all options):", bold=True)
+    try:
+        while True:
+            txt = click.prompt("", prompt_suffix="> ").strip()
+
+            if txt == "":
+                continue
+            else:
+                txt_arr = txt.split(" ")
+                commands.commands(txt_arr, n)
+    except Exception:
+        click.secho("\nGood bye!", fg="green", bold=True)
+        click.clear()
+
+
+def load_prerequisites(spinner, debug, verbose):
+    click.secho("· Loading settings... ", bold=True)
+    with spinner:
+        load_settings_from_cli()
+    print("", end="\x1b[1K\r")
+    click.secho("---> DONE", fg="green", bold=True)
+
+    click.secho("· Waking up the robot... ", bold=True)
+    with spinner:
+        setup_logger(debug, verbose)
+    print("", end="\x1b[1K\r")
+    click.secho("---> DONE", fg="green", bold=True)
+
+
+def load_selector(spinner):
+    click.secho("· Loading models... ", bold=True)
+    with spinner:
+        n = ModuleSelector()
+    print("", end="\x1b[1K\r")
+    click.secho("---> DONE", fg="green", bold=True)
+    click.secho("· Natural language processing ready!", bold=True)
+
+    return n
+
+
+def setup_logger(debug, verbose):
+    try:
+        if debug is not None and verbose:
+            click.echo("[WARNING]: Verbose and debug output are both enabled. Ignoring verbose flag!", err=True)
+
+        if debug is not None:
+            init_logger(debug)
+        elif verbose:
+            init_logger(logging.INFO)
+        else:
+            init_logger(logging.WARNING)
+    except ValueError:
+        init_logger(logging.WARNING)
+        click.echo("[WARNING]: Could not parse debug flag. Using default log settings.", err=True)
+
+
+def init_logger(level):
     # create logger
     logger = logging.getLogger()
     logger.setLevel(level)
@@ -27,41 +95,10 @@ def setup_logger(level):
     logger.addHandler(ch)
 
 
-@plac.annotations(
-    debug=("Set the debug logging level which will be output", "option", "d", str),
-    verbose=("Enable verbose output", "flag", "v"),
-)
-def cli(debug, verbose):
-    try:
-        if debug is not None and verbose:
-            print("WARNING: Verbose and debug output are both enabled. Ignoring verbose flag!")
-
-        if debug is not None:
-            setup_logger(debug)
-        elif verbose:
-            setup_logger(logging.INFO)
-        else:
-            setup_logger(logging.WARNING)
-    except ValueError:
-        setup_logger(logging.WARNING)
-        print("WARNING: Could not parse debug flag. Using default log settings.")
-
-    spin = spinner.Spinner()
-    spin.set_message("Loading settings and nlp models...")
-    with spin:
-        load_settings_from_cli()
-        n = ModuleSelector()
-        print("\nNatural language processing ready!")
-
-    while True:
-        txt = sys.stdin.readline().strip()
-
-        if txt == "":
-            continue
-        else:
-            txt_arr = txt.split(" ")
-            commands.commands(txt_arr, n)
+def supress_warnings():
+    warnings.filterwarnings("ignore", message=r"\[W008\]", category=UserWarning)
 
 
 if __name__ == "__main__":
-    plac.call(cli)
+    supress_warnings()
+    cli()
