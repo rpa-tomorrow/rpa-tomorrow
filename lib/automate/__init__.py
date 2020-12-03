588937283d737bc4370c6ca011e3741975b4fb67
Viktor From <viktor.from91@gmail.com>
2020-12-03 15:39:07 +0100

    Fix potential bug, NoneType 'question'

--- lib/automate/__init__.py
+++ lib/automate/__init__.py
@@ -85,6 +85,8 @@ class Automate:
 
         followup = instance.prepare(SETTINGS["nlp_models"], text, sender)
 
+        if followup == None:
+            return handle_response(followup)
         if self.response_callback:
             return self.response_callback(text, instance, followup)
         return handle_response(followup)
