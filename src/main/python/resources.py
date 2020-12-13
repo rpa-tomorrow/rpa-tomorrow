a86ee311191d54141ef212ab52c41eb3c7e9798a
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-12-13 20:19:18 +0100

    formatted code

--- src/main/python/resources.py
+++ src/main/python/resources.py
@@ -10099,7 +10099,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x76\x5b\xcb\xf9\x6d\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -10107,10 +10107,13 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
