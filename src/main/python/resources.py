c37f125861477becb425ae80b6b60e41f54afd00
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-12-13 14:24:23 +0100

    added license info to info view

--- src/main/python/resources.py
+++ src/main/python/resources.py
@@ -10086,7 +10086,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x76\x51\x52\x35\xb1\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -10094,10 +10094,13 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
